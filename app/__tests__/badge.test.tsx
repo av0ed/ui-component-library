678fb7a19662a155e9888ede8fcabec0d387a044
jlong <codeberg.pumen@simplelogin.com>
2024-08-05 17:51:21 -0500

    add badge testing

--- app/__tests__/badge.test.tsx
+++ app/__tests__/badge.test.tsx
@@ -1,17 +1,52 @@
 import { render, screen } from "@testing-library/react";
+import { axe } from "jest-axe";
 import { describe, it, expect } from "vitest";
 import Badge from "../_components/badge";
 
 describe("Badge", () => {
-  it("renders the badge with the correct text", () => {
-    render(<Badge classes="badge--sm badge--primary" text="Label" />);
-    const badgeElement = screen.getByText("Label");
-    expect(badgeElement).toBeInTheDocument();
+  it("sets default text", () => {
+    render(<Badge />);
+    expect(screen.getByText("Label")).toBeInTheDocument();
   });
 
-  it("applies the correct class", () => {
-    render(<Badge classes="badge--sm badge--danger" text="Label" />);
-    const badgeElement = screen.getByText("Label");
-    expect(badgeElement).toHaveClass("badge--sm badge--danger");
+  it("sets default classes", () => {
+    render(<Badge />);
+    expect(screen.getByText("Label")).toHaveClass("badge--md badge--neutral");
+  });
+
+  it("overrides default classes when classes are supplied", () => {
+    render(<Badge classes="badge--lg badge--primary" />);
+    expect(screen.getByText("Label")).not.toHaveClass(
+      "badge--md badge--neutral",
+    );
+  });
+
+  it("does not set role by default", () => {
+    render(<Badge />);
+    expect(screen.getByText("Label")).not.toHaveAttribute("role");
+  });
+
+  it("sets role with prop", () => {
+    render(<Badge role="status" />);
+    expect(screen.getByText("Label")).toHaveRole("status");
+  });
+
+  it("does not set aria-label by default", () => {
+    render(<Badge />);
+    expect(screen.getByText("Label")).not.toHaveAttribute("aria-label");
+  });
+
+  it("sets aria-label with prop", () => {
+    render(<Badge ariaLabel="Status Badge" />);
+    expect(screen.getByText("Label")).toHaveAttribute(
+      "aria-label",
+      "Status Badge",
+    );
+  });
+
+  it("has no accessibility violations", async () => {
+    const { container } = render(<Badge />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
   });
 });
--- app/_components/badge.tsx
+++ app/_components/badge.tsx
@@ -1,8 +1,19 @@
 interface Props {
-  classes: string;
-  text: string;
+  ariaLabel?: string;
+  classes?: string;
+  role?: string;
+  text?: string;
 }
 
-export default function Badge({ classes, text }: Props) {
-  return <span className={classes}>{text}</span>;
+export default function Badge({
+  ariaLabel,
+  classes = "badge--md badge--neutral",
+  role,
+  text = "Label",
+}: Props) {
+  return (
+    <span className={classes} role={role} aria-label={ariaLabel}>
+      {text}
+    </span>
+  );
 }
